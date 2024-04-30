118832a72b36927a2b79c88e97c2b705223726ce
Abderrahimself <abdemabrouk01@gmail.com>
2024-04-30 23:34:39 +0100

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $incommingFiels = $request->validate([
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        // $incommingFiels['password'] = bcrypt($incommingFiels['password']);
+
+        User::create($incommingFiels);
+        return "Hello from register page";
+    }
+}
